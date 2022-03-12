c76e4d1f6d9d57296cdac839d1d445dc8c3824a0
vaibhav dhiman <princedhiman70@gmail.com>
2022-03-12 23:01:15 +0530

    delete, update task

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,13 +8,19 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./task-item.component.css'],
 })
 export class TaskItemComponent implements OnInit {
-  @Input() task: Task | undefined;
+  @Input()
+  task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
   constructor() {}
 
   ngOnInit(): void {}
 
-  onDelete() {
-    console.log('delete called');
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
   }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/Task';
 // import { TASKS } from 'src/app/mock-tasks';
 import { TaskService } from 'src/app/services/task.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
@@ -13,7 +14,17 @@ export class TasksComponent implements OnInit {
   constructor(private readonly taskService: TaskService) {}
 
   ngOnInit(): void {
-    // this.tasks = this.taskService.getTasks();
     this.taskService.getTasks().subscribe((data) => (this.tasks = data));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => {
+      this.tasks = this.tasks?.filter((t) => t.id !== task.id);
+    });
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTask(task).subscribe();
   }
 }
